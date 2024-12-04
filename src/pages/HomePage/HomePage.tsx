2efec97307e0c88f805ee2398b1114a4cedaecb1
a.makarov <makarofalex@gmail.com>
2024-12-04 15:31:11 +0300

    feat(default-var): create default cocktail var

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,10 +2,9 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { CocktailDetails } from '../../features/cocktail/ui/CocktailDetails';
 import { CocktailMenu } from '../../features/cocktail/ui/CocktailMenu';
 import { NotFoundPage } from '../../features/cocktail/ui/NotFoundPage/NotFoundPage';
+import { DEFAULT_COCKTAIL } from '../../shared/config/menu';
 import styles from './HomePage.module.scss';
 export const HomePage = () => {
-  const defaultCocktail = 'margarita';
-
   return (
     <main className={styles.main}>
       <div className={styles.menu}>
@@ -13,7 +12,7 @@ export const HomePage = () => {
       </div>
       <div className={styles.content}>
         <Routes>
-          <Route path='/' element={<Navigate to={`/${defaultCocktail}`} />} />
+          <Route path='/' element={<Navigate to={`/${DEFAULT_COCKTAIL}`} />} />
           <Route path='/:cocktailCode' element={<CocktailDetails />} />
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
--- src/shared/config/menu.ts
+++ src/shared/config/menu.ts
@@ -1,1 +1,2 @@
 export const MENU_ITEMS = ['margarita', 'mojito', 'a1', 'kir'] as const;
+export const DEFAULT_COCKTAIL = MENU_ITEMS[0];
